c9c61c05a77051e175b15071375e27e0d336881a
Simon Gaudek <simon.gaudek@frauscher.com>
2022-06-15 09:25:51 +0200

    fix(types): Property names with slashes
    If a property name of an interface contains slashes, it has to be wrapped into quotes.

--- src/types/base.type.ts
+++ src/types/base.type.ts
@@ -148,10 +148,9 @@ export abstract class BaseType<O = unknown> {
         types: BaseType[],
         prefix = ""
     ): morph.PropertySignatureStructure {
-        const fieldName =
-            element.canBeNull || element.type === Type.Association
-                ? `${name}?`
-                : name;
+        let fieldName = name;
+        if (fieldName.includes("/")) fieldName = `"${fieldName}"`;
+        if (element.canBeNull || element.type === Type.Association) fieldName = `${fieldName}?`;
 
         let fieldType = "unknown";
         if (element.enum) {
@@ -161,6 +160,7 @@ export abstract class BaseType<O = unknown> {
         } else {
             fieldType = this.cdsElementToType(element, types, prefix);
         }
+
 
         return {
             kind: morph.StructureKind.PropertySignature,
