913939651de8c9640b14758ce5f9dffdc01bc38c
thisisevanfox <johannes.reimann@outlook.com>
2022-11-09 12:10:34 +0100

    feat(types): Make texts attributes optional like localized attributes

--- src/types/base.type.ts
+++ src/types/base.type.ts
@@ -151,7 +151,7 @@ export abstract class BaseType<O = unknown> {
     ): morph.PropertySignatureStructure {
         let fieldName = name;
         if (fieldName.includes("/")) fieldName = `"${fieldName}"`;
-        if (element.canBeNull || element.type === Type.Association)
+        if (element.canBeNull || element.type === Type.Association || name === "texts")
             fieldName = `${fieldName}?`;
 
         let fieldType = "unknown";
